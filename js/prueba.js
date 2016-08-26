ad51fbb2b94265a50de79c91676d4eb5ebf3fe27
yeiny gonzalez cifuentes <yeinygonzalezcifuentes@gmail.com>
2016-08-25 20:42:30 -0500

    condicion de par e impar

---
+++ js/prueba.js
@@ -0,0 +1,19 @@
+    var n= parseInt(prompt("Ingrese un número"));
+
+    if (n %2 == 0 && n < 5){
+    	document.write(" Good!");
+   }
+    else if (n %2 == 0 && n >=6 && n <= 20){
+    	document.write(" Great!");
+    }
+    else if (n %2 == 0 && n > 20){
+    	document.write(" Perfect!");}
+
+    else{
+    	document.write(" Odd!");}
+
+
+
+
+
+
